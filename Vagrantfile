fd12bf38d4f0c1a9d64e5a27be3789772b8b18e8
Dave Fisher <fishd72@users.noreply.github.com>
2020-03-30 15:31:32 +0100

    Updated and streamlined playbooks

--- Vagrantfile
+++ Vagrantfile
@@ -11,7 +11,6 @@ Vagrant.configure("2") do |config|
      apt-add-repository ppa:ansible/ansible
      apt-get update
      apt-get install -y ansible
-     eval "$(ssh-agent -s)"
    SHELL
    config.vm.provision "file", source: "~/.ssh", destination: "$HOME/.ssh"
 end
--- files/files/status.py
+++ files/files/status.py
@@ -1,23 +1,19 @@
+#!/usr/bin/env python
 from gpiozero import PingServer, StatusZero
 from gpiozero.tools import negated
 from signal import pause
 
 sz = StatusZero('router', 'google', 'NAS')
 
-router = PingServer('192.168.178.1')
-google = PingServer('8.8.8.8')
-NAS = PingServer('192.168.178.64')
+statuses = {
+    sz.router: PingServer('192.168.178.1'),
+    sz.google: PingServer('8.8.8.8'),
+    sz.NAS: PingServer('192.168.178.64'),
+}
 
-sz.router.green.source = router.values
-sz.router.green.source_delay = 60
-sz.router.red.source = negated(sz.router.green.values)
-
-sz.google.green.source = google.values
-sz.google.green.source_delay = 60
-sz.google.red.source = negated(sz.google.green.values)
-
-sz.NAS.green.source = NAS.values
-sz.NAS.green.source_delay = 60
-sz.NAS.red.source = negated(sz.NAS.green.values)
+for strip, server in statuses.items():
+    strip.green.source = server.values
+    strip.green.source_delay = 60
+    strip.red.source = negated(strip.green.values)
 
 pause()
